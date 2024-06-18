19102f6d0a40af85f11488f909d8c5cb0cbc6f39
fujunchao <1205721782@qq.com>
2024-06-18 08:46:22 +0800

    Update run.py

--- run.py
+++ run.py
@@ -585,6 +585,7 @@ async def activation_code(access_token, captcha, xid, in_code):
 
 
 async def main():
+    count = 0
     # print('本脚本是固定滑动次数,多次碰撞验证版！！')
     # print('成功与否全凭运气, 可能几次就成功, 也可能几十次都不成功, 请自行测试, 祝使用愉快!')
     for _ in range(2):
@@ -615,6 +616,7 @@ async def main():
         init2_response = await init2(xid, signup_response['access_token'], signup_response['sub'], sign, current_time)
         activation = await activation_code(signup_response['access_token'], init2_response['captcha_token'], xid,
                                            incode)
+        count += 1
         end_time = time.time()
         run_time = f"{(end_time - start_time):.2f}"
         if activation['add_days'] == 5:
@@ -622,15 +624,13 @@ async def main():
         else:
             print(f'邀请码: {incode} ==> 邀请失败, 用时: {run_time} 秒')
       #  input('按回车键再次邀请!!!')
-       # await main()
-        await asyncio.sleep(5)
-        exit(0)
+        if count >= 2:  # 判断是否达到两次运行
+            return  # 退出函数
+        await asyncio.sleep(30)  # 添加30秒延时
+        await main()
     except Exception as e:
         print(f'异常捕获:{e}')
         print('请检查网络环境,(开启科学上网)重试!!!')
        # input('按回车键重试!!!')
-       # await main()
-        await asyncio.sleep(5)
-        exit(0)
-
+        await main()
 asyncio.run(main())
