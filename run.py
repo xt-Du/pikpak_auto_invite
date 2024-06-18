685e4acfd4c77a03dad5e46987d1fa7e289210b3
fujunchao <1205721782@qq.com>
2024-06-18 09:02:24 +0800

    Update run.py

--- run.py
+++ run.py
@@ -624,7 +624,7 @@ async def main():
             print(f'邀请码: {incode} ==> 邀请失败, 用时: {run_time} 秒')
       #  input('按回车键再次邀请!!!')
         if count >= 2:  # 判断是否达到两次运行
-            return  # 退出函数
+            exit(0)  # 退出函数
         await asyncio.sleep(30)  # 添加30秒延时
         await main()
     except Exception as e:
@@ -632,7 +632,7 @@ async def main():
         print('请检查网络环境,(开启科学上网)重试!!!')
        # input('按回车键重试!!!')
         if count >= 2:  # 判断是否达到两次运行
-            return  # 退出函数
+            exit(0)  # 退出函数
         await asyncio.sleep(30)
         await main()
 asyncio.run(main())
